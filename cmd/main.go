268102432f35e26883304488b78a1f77aea57e90
eloquent <woodmanjd32@gmail.com>
2025-05-07 22:47:13 -0400

    continue work

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,8 @@ import (
 	"golang.org/x/term"
 )
 
+const Version = "1.0.0"
+
 // Ctrl+c sets bits 5 & 6 of c to 0
 // Use & to convert c to Ctrl-c
 func CtrlKey(c rune) rune {
@@ -21,8 +23,10 @@ type Editor struct {
 	Out  *os.File
 	Buf  *bytes.Buffer
 	Exit bool
-	Rows int
+	Rows int // Window Size
 	Cols int
+	Cx   int // Cursor Position
+	Cy   int
 }
 
 func (e *Editor) Flush() {
@@ -34,6 +38,11 @@ func (e *Editor) ReadKey() (rune, error) {
 	c, _, err := e.In.ReadRune()
 	if err != nil {
 		return 0, err
+	}
+
+	// TODO: read 2 more chars and set deadline to 100 ms
+	// handle timeout error --> return Esc
+	if c == vt100.Esc {
 	}
 
 	// fmt.Printf("%d: %c\n\r", c, c)
@@ -52,6 +61,14 @@ func (e *Editor) ProcessKey() error {
 		e.ClearScreen()
 		e.Flush()
 		e.Exit = true
+	case 'h':
+		e.Cx--
+	case 'j':
+		e.Cy++
+	case 'k':
+		e.Cy--
+	case 'l':
+		e.Cx++
 	default:
 		fmt.Printf("%d: %c\n\r", c, c)
 	}
@@ -60,8 +77,8 @@ func (e *Editor) ProcessKey() error {
 }
 
 func (e *Editor) ClearScreen() {
-	eid := vt100.EraseInDisplay{Arg: vt100.EIDAll}
-	eid.Write(e.Buf)
+	ed := vt100.EraseInDisplay{Arg: vt100.EDAll}
+	ed.Write(e.Buf)
 }
 
 func (e *Editor) MoveCursorTopLeft() {
@@ -69,10 +86,27 @@ func (e *Editor) MoveCursorTopLeft() {
 	cp.Write(e.Buf)
 }
 
+func (e *Editor) HideCursor() {
+	rm := vt100.ResetMode{Arg: vt100.ModeCursorVisible}
+	rm.Write(e.Buf)
+}
+
+func (e *Editor) ShowCursor() {
+	sm := vt100.SetMode{Arg: vt100.ModeCursorVisible}
+	sm.Write(e.Buf)
+}
+
 func (e *Editor) RefreshScreen() {
-	e.ClearScreen()
-	e.DrawRows()
+	e.HideCursor()
+	// why do you need to do this to draw the screen?
+	// removing it results in weird behavior when moving the cursor
 	e.MoveCursorTopLeft()
+	e.DrawRows()
+
+	cp := vt100.CursorPosition{Row: e.Cy + 1, Column: e.Cx + 1}
+	cp.Write(e.Buf)
+
+	e.ShowCursor()
 
 	e.Flush()
 }
@@ -83,8 +117,19 @@ func (e *Editor) Die(err error) {
 }
 
 func (e *Editor) DrawRows() {
+	el := vt100.EraseInLine{Arg: vt100.ELPosToEnd}
 	for i := range e.Rows {
 		e.Buf.WriteString("~")
+
+		if i == e.Rows/3 {
+			msg := "goeditor -- " + Version
+			for range (e.Cols - len(msg)) / 2 {
+				e.Buf.WriteString(" ")
+			}
+			e.Buf.WriteString(msg)
+		}
+
+		el.Write(e.Buf)
 		if i < e.Rows-1 {
 			e.Buf.WriteString("\n\r")
 		}
@@ -122,7 +167,7 @@ func main() {
 	}
 	defer term.Restore(fd, oldState)
 
-	rows, cols, err := term.GetSize(fd)
+	cols, rows, err := term.GetSize(fd)
 	if err != nil {
 		e.Die(err)
 		return
--- internal/vt100/vt100.go
+++ internal/vt100/vt100.go
@@ -7,7 +7,10 @@ import (
 	"io"
 )
 
-const EscSeq = "\x1b["
+const (
+	Esc    = '\x1b'
+	EscSeq = "\x1b["
+)
 
 // TODO: if performance is a concern, use a string builder
 func write(buf *bytes.Buffer, format string, args ...any) {
@@ -69,20 +72,36 @@ func (cmd *CursorPositionReport) Read(r io.Reader) error {
 	return read(r, "%d;%dR", &cmd.Row, &cmd.Column)
 }
 
-type EIDArg int
+type EDArg int
 
 const (
-	EIDPosToEnd   = 0
-	EIDStartToPos = 1
-	EIDAll        = 2
+	EDPosToEnd   = 0
+	EDStartToPos = 1
+	EDAll        = 2
 )
 
 type EraseInDisplay struct {
-	Arg EIDArg
+	Arg EDArg
 }
 
 func (cmd *EraseInDisplay) Write(buf *bytes.Buffer) {
 	write(buf, "%dJ", cmd.Arg)
+}
+
+type ELArg int
+
+const (
+	ELPosToEnd   = 0
+	ELStartToPos = 1
+	ELAll        = 2
+)
+
+type EraseInLine struct {
+	Arg ELArg
+}
+
+func (cmd *EraseInLine) Write(buf *bytes.Buffer) {
+	write(buf, "%dK", cmd.Arg)
 }
 
 type CursorPosition struct {
@@ -92,4 +111,24 @@ type CursorPosition struct {
 
 func (cmd *CursorPosition) Write(buf *bytes.Buffer) {
 	write(buf, "%d;%dH", cmd.Row, cmd.Column)
+}
+
+type ModeArg string
+
+const ModeCursorVisible = "?25"
+
+type SetMode struct {
+	Arg ModeArg
+}
+
+func (cmd *SetMode) Write(buf *bytes.Buffer) {
+	write(buf, "%sh", cmd.Arg)
+}
+
+type ResetMode struct {
+	Arg ModeArg
+}
+
+func (cmd *ResetMode) Write(buf *bytes.Buffer) {
+	write(buf, "%sl", cmd.Arg)
 }
